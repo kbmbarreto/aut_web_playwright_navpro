ea64c920525c7d2b842782c4dc32db46c9fe7d28
cskmacedo <kleber.barreto@msn.com>
2024-10-12 02:11:23 -0300

    Adicionado projeto inicial dos estudos

---
+++ app.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as playwright:
+    # Lauch a browser
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    # Create a new page
+    page = browser.new_page()
+    # Visit the website
+    page.goto("https://navpro.dasa.com.br")
+
+    # Locate a link element with "Entrar" text
+    exames_button = page.get_by_role('link', name="Exames")
+    exames_button.click()
+
+    #Get the url
+    print("Exames:", page.url)
+
+    browser.close()
