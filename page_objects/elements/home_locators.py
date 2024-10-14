f46465b68adc2fedbf7736a17b4efafba6ef4474
cskmacedo <kleber.barreto@msn.com>
2024-10-14 15:02:13 -0300

    locators da pagina principal mapeados

---
+++ page_objects/elements/home_locators.py
@@ -0,0 +1,11 @@
+class HomeLocators:
+    TXT_TITLE = "A plataforma para gestão de exames"
+    LNK_EXAMES = 'role=link[name="Exames"]'
+    LNK_PACIENTES = 'role=link[name="Pacientes"]'
+    LNK_FAQ = 'role=link[name="FAQ"]'
+    BTN_ENTRAR = 'role=link[name="Entrar"]'
+    BTN_CADASTRE_SE = 'role=link[name="Cadastre-se"]'
+    BTN_QUERO_ME_CADASTRAR = 'role=link[name="Quero me cadastrar"]'
+    BTN_DEFINICAO_COOKIES = 'role=button[name="Definições de cookies"]'
+    BTN_COOKIES_ENTENDI = 'role=button[name="Entendi"]'
+    BTN_FECHAR_COOKIES = 'role=button[name="Fechar"]'
--- page_objects/elements/locators.py
+++
@@ -1,3 +0,0 @@
-class NavProPageLocators:
-    TITLE_TEXT = "A plataforma para gestão de exames"
-    EXAMES_LINK = 'role=link[name="Exames"]'
